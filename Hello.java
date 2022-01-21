c15fdde642e0eb8dc08fc6ca91c469da5cbb0a75
KALPIT VYAS <34889228+KalpitVyas@users.noreply.github.com>
2022-01-21 17:13:38 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 class Hello{
 	public static void main(String [] args){
 		System.out.println("HELLO");
+		System.out.println("HELLO");
+		System.out.println("HELLO");
+		System.out.println("HELLO");
 	} //end of main
 } 
